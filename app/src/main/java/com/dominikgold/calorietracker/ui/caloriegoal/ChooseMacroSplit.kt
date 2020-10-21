f0469f0116592abb2370cffe361f149e90d3a4d5
dominikgold <dominik.goldberg@kitchenstories.de>
2020-10-21 17:40:37 +0200

    add common TextDropdownToggle composable

--- app/src/main/java/com/dominikgold/calorietracker/ui/caloriegoal/ChooseMacroSplit.kt
+++ app/src/main/java/com/dominikgold/calorietracker/ui/caloriegoal/ChooseMacroSplit.kt
@@ -1,6 +1,5 @@
 package com.dominikgold.calorietracker.ui.caloriegoal
 
-import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -19,13 +18,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import com.dominikgold.calorietracker.R
 import com.dominikgold.calorietracker.entities.MacroSplit
 import com.dominikgold.calorietracker.theming.textColorDefault
 import com.dominikgold.calorietracker.theming.textColorSubtitle
+import com.dominikgold.calorietracker.ui.common.TextDropdownToggle
 import com.dominikgold.calorietracker.util.Translated
 import com.dominikgold.calorietracker.util.inLightAndDarkTheme
 
@@ -35,20 +34,18 @@ fun ChooseMacroSplit(macroSplitUiModel: MacroSplitUiModel?, onMacroSplitChosen: 
         val dropdownMenuExpanded = remember { mutableStateOf(false) }
         DropdownMenu(
             toggle = {
-                Row(Modifier.fillMaxWidth()) {
-                    Text(
-                        modifier = Modifier.weight(1f),
-                        text = Translated(macroSplitUiModel?.name) ?: Translated(R.string.choose_macro_split_hint),
-                        color = if (macroSplitUiModel == null) textColorSubtitle else textColorDefault,
-                    )
-                    Icon(asset = vectorResource(id = R.drawable.vec_icon_arrow_drop_down), tint = textColorDefault)
-                }
+                TextDropdownToggle(
+                    text = Translated(macroSplitUiModel?.name),
+                    placeholder = Translated(R.string.choose_macro_split_hint),
+                    isExpanded = dropdownMenuExpanded.value,
+                    modifier = Modifier.fillMaxWidth(),
+                )
             },
-            expanded = dropdownMenuExpanded.value,
-            onDismissRequest = { dropdownMenuExpanded.value = false },
             toggleModifier = Modifier.clickable(onClick = { dropdownMenuExpanded.value = !dropdownMenuExpanded.value })
                 .fillMaxWidth()
                 .padding(top = 8.dp, bottom = 8.dp),
+            expanded = dropdownMenuExpanded.value,
+            onDismissRequest = { dropdownMenuExpanded.value = false },
             dropdownModifier = Modifier.fillMaxWidth(),
         ) {
             MacroSplitDropdownItems(onItemSelected = {
@@ -63,16 +60,15 @@ fun ChooseMacroSplit(macroSplitUiModel: MacroSplitUiModel?, onMacroSplitChosen: 
 
 @Composable
 private fun MacroSplitDropdownItems(onItemSelected: (MacroSplit) -> Unit) {
-    MacroSplit.values()
-        .forEach { macroSplit ->
-            DropdownMenuItem(onClick = { onItemSelected(macroSplit) }) {
-                Text(text = Translated(macroSplit.translatableName))
-            }
+    MacroSplit.values().forEach { macroSplit ->
+        DropdownMenuItem(onClick = { onItemSelected(macroSplit) }) {
+            Text(text = Translated(macroSplit.translatableName))
         }
+    }
 }
 
 @Composable
-private fun ColumnScope.MacroSplitValues(macroSplitUiModel: MacroSplitUiModel?) {
+private fun MacroSplitValues(macroSplitUiModel: MacroSplitUiModel?) {
     Row {
         Text(text = Translated(R.string.choose_macro_split_carbohydrates_title), color = textColorDefault)
         Spacer(modifier = Modifier.width(16.dp))
@@ -93,7 +89,7 @@ private fun ColumnScope.MacroSplitValues(macroSplitUiModel: MacroSplitUiModel?) 
 }
 
 @Composable
-@Preview(showBackground = true)
+@Preview
 fun ChooseMacroSplitPreview() {
     inLightAndDarkTheme {
         ChooseMacroSplit(MacroSplitUiModel(MacroSplit.HIGH_CARB, 2000)) {}
---
+++ app/src/main/java/com/dominikgold/calorietracker/ui/common/DropdownToggle.kt
@@ -0,0 +1,50 @@
+package com.dominikgold.calorietracker.ui.common
+
+import androidx.compose.foundation.Icon
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
+import com.dominikgold.calorietracker.R
+import com.dominikgold.calorietracker.theming.textColorDefault
+import com.dominikgold.calorietracker.theming.textColorSubtitle
+import com.dominikgold.calorietracker.util.inLightAndDarkTheme
+
+@Composable
+fun TextDropdownToggle(text: String?, placeholder: String?, isExpanded: Boolean, modifier: Modifier = Modifier) {
+    val iconResource = if (isExpanded) R.drawable.vec_icon_arrow_drop_up else R.drawable.vec_icon_arrow_drop_down
+    Row(modifier) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = text ?: placeholder ?: "",
+            color = if (text == null) textColorSubtitle else textColorDefault,
+        )
+        Icon(asset = vectorResource(id = iconResource), tint = textColorDefault)
+    }
+}
+
+@Preview
+@Composable
+fun TextDropdownTogglePreview() {
+    inLightAndDarkTheme {
+        TextDropdownToggle(
+            text = "Example",
+            placeholder = null,
+            isExpanded = false,
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextDropdownToggle(
+            text = null,
+            placeholder = "placeholder",
+            isExpanded = false,
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+}
